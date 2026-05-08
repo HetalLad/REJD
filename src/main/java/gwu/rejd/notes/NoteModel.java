f5d9dd006aea3e4191050e7dd1e1d869be40181f
Anirwin1303 <122205899+Anirwin1303@users.noreply.github.com>
2026-05-08 19:48:41 -0400

    Update NoteModel.java
    Updated Documentation

--- src/main/java/gwu/rejd/notes/NoteModel.java
+++ src/main/java/gwu/rejd/notes/NoteModel.java
@@ -1,10 +1,21 @@
+/*
+File name: NoteModel.java
+Authors: Anirvinna Jain, Hetal Lad, Saptorshee Nag 
+Description: The NoteModel class is intialized here
+*/
+
+// Package info
 package gwu.rejd.notes;
 
+// Imports
 import java.util.ArrayList;
 import java.util.List;
 
+/*
+Class NoteModel initializes the NoteModel object.
+*/
 public class NoteModel {
-    public String id;
+    public String id;  
     public String componentId;
     public String author;
     public String timestamp;
