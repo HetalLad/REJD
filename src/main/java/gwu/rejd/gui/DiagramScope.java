c35eb95fc95847ee46d213658fbe35f96ba3a5d5
Anirwin1303 <122205899+Anirwin1303@users.noreply.github.com>
2026-05-08 22:15:39 -0400

    Update DiagramScope.java
    Updating documentation

--- src/main/java/gwu/rejd/gui/DiagramScope.java
+++ src/main/java/gwu/rejd/gui/DiagramScope.java
@@ -1,5 +1,15 @@
+/*
+Filename: DiagramScope.java
+Author: Anirvinna Jain, Hetal Lad, Saptorshee Nag
+Description: Implements the class to describe the diagram scope.
+*/
+
+// Package info
 package gwu.rejd.gui;
 
+/**
+* Public class to implement the scope of the diagram rendered.
+*/
 public class DiagramScope {
 
     public enum ScopeType {
@@ -10,27 +20,33 @@ public class DiagramScope {
     private final ScopeType type;
     private final String packageName;
 
+    // Constructor
     private DiagramScope(ScopeType type, String packageName) {
         this.type = type;
         this.packageName = packageName;
     }
 
+    // Scope is the enter project
     public static DiagramScope entireProject() {
         return new DiagramScope(ScopeType.ENTIRE_PROJECT, null);
     }
 
+    // Scope is some package
     public static DiagramScope forPackage(String packageName) {
         return new DiagramScope(ScopeType.PACKAGE, packageName);
     }
 
+    // Getter for the scope type
     public ScopeType getType() {
         return type;
     }
 
+    // Getter for the package name
     public String getPackageName() {
         return packageName;
     }
 
+    // Checks if the scope is the entire project
     public boolean isEntireProject() {
         return type == ScopeType.ENTIRE_PROJECT;
     }
