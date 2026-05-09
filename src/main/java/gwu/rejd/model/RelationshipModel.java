bea559ab4dbfcd15bfd99f84a601964a7631387b
Anirwin1303 <122205899+Anirwin1303@users.noreply.github.com>
2026-05-08 20:09:55 -0400

    Update RelationshipModel.java
    Updated documentation

--- src/main/java/gwu/rejd/model/RelationshipModel.java
+++ src/main/java/gwu/rejd/model/RelationshipModel.java
@@ -1,15 +1,27 @@
+/*
+File Name: RelationshipModel.java
+Authors: Anirvinna Jain, Hetal Lad, Saptorshee Nag
+Description: The file describes the relationship models.
+*/
+
+// Package info
 package gwu.rejd.model;
 
+// Import statements
 import gwu.rejd.model.enums.RelationshipKind;
 
 import java.util.Objects;
 
+/**
+* RelationshipModel class defines objects to describe relationship between two objects.
+*/
 public final class RelationshipModel {
 
     private final String sourceFqn;
     private final String targetName;
     private final RelationshipKind kind;
 
+    // Constructor
     public RelationshipModel(String sourceFqn, String targetName, RelationshipKind kind) {
         this.sourceFqn = Objects.requireNonNull(sourceFqn);
         this.targetName = Objects.requireNonNull(targetName);
@@ -20,6 +32,7 @@ public final class RelationshipModel {
     public String targetName() { return targetName; }
     public RelationshipKind kind() { return kind; }
 
+    // Comparing two objects
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -30,11 +43,13 @@ public final class RelationshipModel {
                 && kind == other.kind;
     }
 
+    // Hahses the data
     @Override
     public int hashCode() {
         return Objects.hash(sourceFqn, targetName, kind);
     }
 
+    // String representation of the object
     @Override
     public String toString() {
         return sourceFqn + " -[" + kind + "]-> " + targetName;
