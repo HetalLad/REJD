6b22c5cc671f857511a430ab84aacfc938d2d207
SaptorsheeNag <83335027+SaptorsheeNag@users.noreply.github.com>
2026-05-08 22:30:19 -0400

    Update RelationshipScopeFilter.java

--- src/main/java/gwu/rejd/gui/RelationshipScopeFilter.java
+++ src/main/java/gwu/rejd/gui/RelationshipScopeFilter.java
@@ -1,3 +1,10 @@
+/*
+ * File Name: RelationshipScopeFilter.java
+ * Authors: Anirvinna Jain, Hetal Lad, Saptorshee Nag
+ * Description: Filters relationships based on the currently
+ * selected diagram scope/package.
+ */
+
 package gwu.rejd.gui;
 
 import gwu.rejd.model.RelationshipModel;
@@ -6,9 +13,16 @@ import gwu.rejd.util.TypeNameUtils;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * Used to limit visible relationships when a package-level
+ * diagram scope is selected.
+ */
+
 public class RelationshipScopeFilter {
 
     public List<RelationshipModel> filter(DiagramScope scope, List<RelationshipModel> relationships) {
+
+        // Show everything when viewing the entire project
         if (scope == null || scope.isEntireProject()) {
             return relationships;
         }
